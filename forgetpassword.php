ae33a22e69d6b1feaa1f668e23e8c0a77181d575
Sarahjpxx <63956694+Sarahjpxx@users.noreply.github.com>
2020-05-21 11:43:29 +0100

    Add files via upload

--- forgetpassword.php
+++ forgetpassword.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php session_start ();?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -22,44 +17,58 @@ and open the template in the editor.
         } catch (Exception $e) {
             $message = $e->getMessage();
         }
-        
+
         if (isset($_POST['recover'])) {
             $login_username = $_POST['login_username'];
+            $email = $_POST['email'];
             //The password is encrypted
-            $email=$_POST['email'];
+            $_SESSION ['username']=$login_username;
+            $_SESSION ['email']=$email;
             
+
             echo $login_username;
             echo $email;
-           
+
+
             $stmt = $pdo->prepare("SELECT userName, email FROM member WHERE userName =:username and email =:email");
-            $stmt->bindParam(":username", $login_username);
-            $stmt->bindParam(":email", $email);
-            
-          
+            $stmt->bindParam(":username", $_SESSION['username']);
+            $stmt->bindParam(":email", $_SESSION['email']);
+
+
             $stmt->execute();
             $count = $stmt->rowCount();
-            
-             if ($count > 0) {
-             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-             $stmt1 = $pdo->prepare("UPDATE member SET passwords =:password WHERE userName =:username");
-              $stmt1->bindParam(":username", $login_username);
-            $stmt1->bindParam(":password", $password);
-            
-            $stmt1->execute();
-            $count1 = $stmt1->rowCount();
-            
-            if ($count1>0){
-                
-            echo "success";}
-            
-           else {echo "Try again";}
-            
-            
-          
-                
+
+
+
+            if ($count > 0 && (isset($_POST['recover']))) {
+                ?>
+
+                <form action = "" method = "POST">
+
+                    <input type='password' name='password'>
+
+
+
+                    <button type='submit' name='recover'>Change Password</button></form>
+
+                <?php
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                echo $_SESSION['username'];
+                $stmt1 = $pdo->prepare("UPDATE member SET passwords =:password WHERE userName =:username");
+                $stmt1->bindParam(":username", $_SESSION['username']);
+                $stmt1->bindParam(":password", $password);
+
+                $stmt1->execute();
+                $count1 = $stmt1->rowCount();
+
+                if ($count1 > 0) {
+                    
+                } else {
+                    echo "Try again";
+                }
             } else {
                 echo "Username does not exist.";
-        }
+            }
         }
         ?>
 
@@ -70,10 +79,14 @@ and open the template in the editor.
             Email address:
 
             <input type='email' name='email' required>
-            <input type='password' name='password'>
-            <button type='submit' name='recover'>Recover</button>
-            
+
+            <button type='submit' name='recover'>Submit</button>
+
+
+
+
+
         </form>
-            
-            </body>
-            </html>
+
+    </body>
+</html>
